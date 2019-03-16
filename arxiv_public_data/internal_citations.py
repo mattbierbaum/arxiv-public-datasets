968d4e13f72ee150296307450d1b936545835583
Colin Clement <colin.clement@gmail.com>
2019-03-16 17:51:11 -0400

    test for cleaning regex arxiv citations

--- arxiv_public_data/internal_citations.py
+++ arxiv_public_data/internal_citations.py
@@ -8,77 +8,82 @@ import gzip
 import numpy as np
 from multiprocessing import Pool
 
-from regex_arxiv import REGEX_ARXIV_STRICT, REGEX_ARXIV_FLEXIBLE
+from arxiv_public_data.regex_arxiv import REGEX_ARXIV_FLEXIBLE, clean
 
-RE_REMOVE_SPLIT = re.compile(r'(.*)\/\d+\/(.*)')
-version_pat = re.compile(r"v(\d+).txt")
 
-def convert_filename_id(s, root):
-    s = s.split(root)[1]
-    s = '/'.join(RE_REMOVE_SPLIT.match(s).groups())
-    if s.startswith('arxiv'):
-        s = s.split('arxiv/')[1].split('v')[0]
-    else:
-        s = s.split('v')[0]
-    return s
+OUTDIR = '/pool0/arxiv/full-text'
+RE_FLEX = re.compile(REGEX_ARXIV_FLEXIBLE)
+RE_OLDNAME_SPLIT = re.compile(r"([a-z\-]+)(\d+)")
 
-def all_articles(root):
-    all_text_files = sorted(glob.glob(os.path.join(root, '*/*/*.txt*')))
-    return [convert_filename_id(s) for s in all_text_files]
+def path_to_id(path):
+    """ Convert filepath name of ArXiv file to ArXiv ID """
+    name = os.path.splitext(os.path.basename(path))[0]
+    if '.' in name:  # new  ID
+        return name 
+    split = [a for a in RE_OLDNAME_SPLIT.split(name) if a]
+    return "/".join(split)
 
-def read_article(a):
-    fname = read_article(a)
-
-    if not fname:
-        return ''
-
-    try:
-        with gzip.open(fname) as fn:
-            txt = fn.read()
-    except:
-        with open(fname) as fn:
-            txt = fn.read()
-
-    try:
-        txt = txt.decode('utf-8')
-    except:
-        pass
-    return txt
-
-def extract_references(txt):
+def all_articles(directory=OUTDIR):
+    """ Find all *.txt files in directory """
     out = []
-    for matches in re.findall(REGEX_ARXIV_FLEXIBLE, txt):
-        out.extend([a for a in matches if a])
-    return list(set(out))
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            if 'txt' in f:
+                out.append(os.path.join(root, f))
+    return out
 
-def citation_list():
-    cites = {}
-    articles = all_articles()
+def extract_references(filename, pattern=RE_FLEX):
+    """
+    Parameters
+    ----------
+        filename : str
+            name of file to search for pattern
+        pattern : re pattern object
+            compiled regex pattern
 
-    t_start = time.time()
-    for i, article in enumerate(articles):
-        if i % 10 == 0:
-            t_end = time.time()
-            print(i, article, t_end - t_start)
-        text = read_article(article)
-        refs = extract_references(text)
-        cites[article] = refs
-
-    return cites
+    Returns
+    -------
+        citations : list
+            list of found arXiv IDs
+    """
+    out = []
+    for matches in pattern.findall(open(filename, 'r').read()):
+        out.extend([clean(a) for a in matches if a])
+    return out
 
 def citation_list_inner(articles):
+    """ Find references in all the input articles
+    Parameters
+    ----------
+        articles : list of str
+            list of paths to article text
+    Returns
+    -------
+        citations : dict[arXiv ID] = list of arXiv IDs
+            dictionary of articles and their references
+    """
     cites = {}
     for i, article in enumerate(articles):
         try:
-            text = read_article(article)
-            if text:
-                refs = extract_references(text)
-                cites[article] = refs
+            refs = extract_references(article)
+            cites[path_to_id(article)] = refs
         except:
+            print("Error in {}".format(articles))
             continue
     return cites
 
-def citation_list_parallel(N):
+def citation_list_parallel(N=8):
+    """
+    Split the task of checking for citations across some number of processes
+    Parameters
+    ----------
+        N : int
+            number of processes
+    Returns
+    -------
+        citations : dict[arXiv ID] = list of arXiv IDs
+            all arXiv citations in all articles
+    """
     articles = all_articles()
 
     pool = Pool(N)
@@ -88,4 +93,3 @@ def citation_list_parallel(N):
     for c in cites:
         allcites.update(c)
     return allcites
-
