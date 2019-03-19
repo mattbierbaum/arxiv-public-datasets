c00fc6e7912bf02aec4b9669b417e156aca6aa9a
Colin Clement <colin.clement@gmail.com>
2019-03-19 16:11:32 -0400

    added universal-sentence-encoder and improved tf boilerplate

--- arxiv_public_data/embeddings/tf_hub.py
+++ arxiv_public_data/embeddings/tf_hub.py
@@ -11,13 +11,19 @@ import numpy as np
 
 import tensorflow as tf
 import tensorflow_hub as hub
+import sentencepiece as spm
 
 from arxiv_public_data.config import ARXIV_DIR
 
-def embed_strings(strings, filename, batchsize=32,
-                  model_url="https://tfhub.dev/google/elmo/2",
-                  model_kwargs = dict(signature='default', as_dict=True),
-                  dictkey='default'):
+UNIV_SENTENCE_ENCODER_URL = ('https://tfhub.dev/google/'
+                             'universal-sentence-encoder/2')
+
+ELMO_URL = "https://tfhub.dev/google/elmo/2"
+ELMO_KWARGS = dict(signature='default', as_dict=True)
+ELMO_MODULE_KWARGS = dict(trainable=True)
+ELMO_DICTKEY = 'default'
+
+def elmo_strings(strings, filename, batchsize=32):
     """
     Compute and save vector embeddings of lists of strings in batches
     Parameters
@@ -29,29 +35,101 @@ def embed_strings(strings, filename, batchsize=32,
         (optional)
         batchsize : int
             size of batches
-        model_url : str
-            TensorFlow hub model url
-        model_kwargs : dict
-            Dictionary of kwargs that the model specifies
-        dictkey : str
-            Many models return dicts, this specifies which part to select
     """
     batches = np.array_split(
         np.array(strings), len(strings)//batchsize
     )
-    with tf.Graph().as_default():
-        embd = hub.Module(model_url, trainable=True)
 
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            sess.run(tf.tables_initializer())
+    g = tf.Graph()
+    with g.as_default():
+        module = hub.Module(ELMO_URL, **ELMO_MODULE_KWARGS)
+        text_input = tf.placeholder(dtype=tf.string, shape=[None])
+        embeddings = module(text_input, **ELMO_KWARGS)
+        init_op = tf.group([tf.global_variables_initializer(),
+                            tf.tables_initializer()])
+    g.finalize()
 
-            for i, batch in enumerate(batches):
-                # grab mean-pooling of contextualized word reps
-                embeddings = embd(batch, **model_kwargs)[dictkey]
-                print("Computing/saving batch {}".format(i))
-                with open(filename, 'ab') as fout:
-                    pickle.dump(sess.run(embeddings), fout)
+    with tf.Session(graph=g) as sess:
+        sess.run(init_op)
+
+        for i, batch in enumerate(batches):
+            # grab mean-pooling of contextualized word reps
+            print("Computing/saving batch {}".format(i))
+            with open(filename, 'ab') as fout:
+                pickle.dump(sess.run(
+                    embeddings, feed_dict={text_input: batch}
+                )[ELMO_DICTKEY], fout)
+
+UNIV_SENTENCE_LITE = "https://tfhub.dev/google/universal-sentence-encoder-lite/2"
+
+def get_sentence_piece_model():
+    with tf.Session() as sess:
+        module = hub.Module(UNIV_SENTENCE_LITE)
+        return sess.run(module(signature="spm_path"))
+
+def process_to_IDs_in_sparse_format(sp, sentences):
+    """
+    An utility method that processes sentences with the sentence piece
+    processor
+    'sp' and returns the results in tf.SparseTensor-similar format:
+    (values, indices, dense_shape)
+    """
+    ids = [sp.EncodeAsIds(x) for x in sentences]
+    max_len = max(len(x) for x in ids)
+    dense_shape=(len(ids), max_len)
+    values=[item for sublist in ids for item in sublist]
+    indices=[[row,col] for row in range(len(ids)) for col in range(len(ids[row]))]
+    return (values, indices, dense_shape)
+
+def universal_sentence_encoder_lite(strings, filename, spm_path, batchsize=32):
+    """
+    Compute and save vector embeddings of lists of strings in batches
+    Parameters
+    ----------
+        strings : list(str)
+            list of strings to be embedded
+        filename : str
+            filename to store embeddings            
+        spm_path : str
+            path to sentencepiece model from `get_sentence_piece_model`
+        (optional)
+        batchsize : int
+            size of batches
+    """
+    batches = np.array_split(
+        np.array(strings), len(strings)//batchsize
+    )
+    sp = spm.SentencePieceProcessor()
+    sp.Load(spm_path)
+
+    g = tf.Graph()
+    with g.as_default():
+        module = hub.Module(UNIV_SENTENCE_LITE)
+        input_placeholder = tf.sparse_placeholder(tf.int64, shape=[None, None])
+        embeddings = module(
+            inputs=dict(
+                values=input_placeholder.values, indices=input_placeholder.indices,
+                dense_shape=input_placeholder.dense_shape
+            )
+        )
+        init_op = tf.group([tf.global_variables_initializer(),
+                            tf.tables_initializer()])
+    g.finalize()
+
+    with tf.Session(graph=g) as sess:
+        sess.run(init_op)
+        for i, batch in enumerate(batches):
+            values, indices, dense_shape = process_to_IDs_in_sparse_format(sp, batch)
+            print("Computing/saving batch {}".format(i))
+            emb = sess.run(
+                embeddings, 
+                feed_dict={
+                    input_placeholder.values: values, input_placeholder.indices: indices, 
+                    input_placeholder.dense_shape: dense_shape
+                }
+            )
+            with open(filename, 'ab') as fout:
+                    pickle.dump(emb, fout)
 
 def load_embeddings(filename):
     """
@@ -73,7 +151,8 @@ def load_embeddings(filename):
                 break 
     return np.array(out)
 
-def create_save_embeddings(strings, filename, SAVEDIR=ARXIV_DIR, **kwargs):
+def create_save_embeddings(strings, filename, encoder, encoder_args=(),
+                           encoder_kwargs={}, SAVEDIR=ARXIV_DIR):
     """
     Create vector embeddings of strings and save them to filename
     Parameters
@@ -87,4 +166,4 @@ def create_save_embeddings(strings, filename, SAVEDIR=ARXIV_DIR, **kwargs):
         os.makedirs(filepath)
 
     print("Saving embeddings to {}".format(os.path.join(filepath, filename)))
-    embed_strings(strings, os.path.join(filepath, filename), **kwargs)
+    encoder(strings, os.path.join(filepath, filename), *encoder_args, **encoder_kwargs)
