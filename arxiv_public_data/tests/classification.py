5a6164af66e94cd85cfdb92e6fb109602b568192
Colin Clement <colin.clement@gmail.com>
2019-03-21 12:44:07 -0400

    cocitation, train_fit function, memory improvements

--- arxiv_public_data/tests/classification.py
+++ arxiv_public_data/tests/classification.py
@@ -6,11 +6,33 @@ Classification.py
 import os
 import pickle
 import numpy as np
+from datetime import datetime
 #import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 
 from arxiv_public_data.oai_metadata import load_metadata
-from arxiv_public_data.embeddings.tf_hub import load_embeddings
+from arxiv_public_data.tests.intra_citation import loaddata
+import arxiv_public_data.tests.cocitation_category_feature as features
+
+def load_embeddings(filename):
+    """
+    Loads vector embeddings
+    Parameters
+    ----------
+        filename : str
+            path to vector embeddings saved by `create_save_embeddings`
+    Returns
+    -------
+        embeddings : array_like
+    """
+    out = []
+    with open(filename, 'rb') as fin:
+        while True:
+            try:
+                out.extend(pickle.load(fin))
+            except EOFError as e:
+                break 
+    return np.array(out)
 
 def in_top_n(prob, target, n=5):
     intopn = 0
@@ -20,11 +42,24 @@ def in_top_n(prob, target, n=5):
             intopn += 1
     return intopn/prob.shape[0]
 
+def train_test(model, X_train, y_train, X_test, y_test):
+    model.fit(X_train, y_train)
+    prec = np.mean(model.predict(X_test) == y_test)
+    prob = model.predict_proba(X_test)
+    loglikelihood = np.sum(np.log([p[t] for p, t in zip(prob, y_test)]))
+    perplexity = 2 ** ( - loglikelihood / len(y_test) / np.log(2))
+    top3 = in_top_n(prob, y_test, 3)
+    top5 = in_top_n(prob, y_test, 5)
+    return dict(top1=prec, top3=top3, top5=top5, loglikelihood=loglikelihood,
+                perplexity=perplexity)
+
 EMBDIR = '/pool0/arxiv/embeddings'
 usel_abstract = os.path.join(EMBDIR, 'abstract-embedding-usel-2019-03-19.pkl')
 usel_title = os.path.join(EMBDIR, 'title-embedding-usel-2019-03-19.pkl')
 md_file = ('/home/colin/work/arxiv-public-datasets/data/'
            'oai-arxiv-metadata-2019-03-01.json.gz')
+adj_file = ('/home/colin/work/arxiv-public-datasets/data/'
+            'internal-references-pdftotext.json.gz')
 
 def maincat(name):
     if '.' in name:
@@ -36,74 +71,101 @@ def shuffle(arr, seed=14850):
     rng = np.random.RandomState(seed)
     rng.shuffle(arr)
 
-abstract_vec = load_embeddings(usel_abstract)
-title_vec = load_embeddings(usel_title)
-order = np.arange(len(title_vec))
-shuffle(abstract_vec)
-shuffle(title_vec)
-shuffle(order)
+def ids_cats(md_file, subcats=True):
+    md = load_metadata(md_file)
+    ids = np.array([m['id'] for m in md], dtype='object')
+    shuffle(ids)
+    if subcats:
+        cats = np.array([m['categories'][0].split()[0] for m in md],
+                        dtype='object')
+    else:
+        cats = np.array([maincat(m['categories'][0].split()[0]) for m in md],
+                        dtype='object')
+    shuffle(cats)
+    return ids, cats
 
-md = load_metadata(md_file)
+if __name__ == "__main__":
+    
+    adj = loaddata(adj_file)
+    ids, cats = ids_cats(md_file, subcats=True) 
+    
+    scats = list(set(cats))
+    labels = {c: l for l, c in enumerate(scats)}
+    target = [labels[c] for c in cats]
+    
+    train_size = 1200000
+    
+    ids_train = ids[:train_size]
+    ids_test = ids[train_size:]
+    
+    target_train = target[:train_size]
+    target_test = target[train_size:]
 
-cats = [maincat(m['categories'][0].split()[0]) for m in md]
-shuffle(cats)
+    # Features containing cocitation category information
+    mc_train, mc_test = features.cocitation_feature(adj, ids_train, ids_test,
+                                                    target_train, target_test)
 
-scats = list(set(cats))
-labels = {c: l for l, c in enumerate(scats)}
-target = [labels[c] for c in cats]
+    lr = LogisticRegression(solver='lbfgs', multi_class='multinomial',
+                            verbose=1, max_iter=200)
+    results = {}
+    
+    ## First fit on just titles
+    title_vec = load_embeddings(usel_title)
+    shuffle(title_vec)
+    title_train = title_vec[:train_size]
+    title_test = title_vec[train_size:]
 
-train_size = 1200000
-target_train = target[:train_size]
-target_test = target[train_size:]
-title_train = title_vec[:train_size]
-title_test = title_vec[train_size:]
-abstract_train = abstract_vec[:train_size]
-abstract_test = abstract_vec[train_size:]
+    print('Fitting title vectors')
+    results['titles'] = train_test(lr, title_train, target_train, title_test,
+                                   target_test)
+    print(results['titles'])
+    print('title vectors done!')
+    
+    ### Next fit on just the abstracts
+    abstract_vec = load_embeddings(usel_abstract)
+    shuffle(abstract_vec)
+    abstract_train = abstract_vec[:train_size]
+    abstract_test = abstract_vec[train_size:]
 
-combined_train = np.concatenate([title_train, abstract_train], axis=1)
-combined_test = np.concatenate([title_test, abstract_test], axis=1)
+    print('Fitting abstract vectors')
+    results['abstracts'] = train_test(lr, abstract_train, target_train,
+                                      abstract_test, target_test)
+    print(results['abstracts'])
+    print('abstract vectors done!')
+    
+    # Now on the combination of titles and abstracts
+    title_abstract_train = np.concatenate([title_train, abstract_train], axis=1)
+    del title_train
+    title_abstract_test = np.concatenate([title_test, abstract_test], axis=1)
+    del title_test
+    print('Fitting abstract + title vectors')
+    results['abstract+titles'] = train_test(
+        lr, title_abstract_train, target_train, title_abstract_test, target_test
+    )
+    print(results['abstract+titles'])
+    print('abstract + title vectors done!')
 
-lr = LogisticRegression(solver='lbfgs', multi_class='multinomial', verbose=1)
+    # Next fit on the cocitation features
+    print('Fitting cocitation vectors')
+    results['cocitation'] = train_test(lr, mc_train, target_train,
+                                       mc_test, target_test)
+    print(results['cocitation'])
+    print('cocitation vectors done!')
 
-results = {}
-
-## First fit on just titles
-title_results = {}
-print('Fitting title vectors')
-lr.fit(title_train, target_train)
-prec = np.mean(lr.predict(title_test) == target_test)
-prob = lr.predict_proba(title_test)
-loglikelihood = np.sum(np.log([p[t] for p, t in zip(prob, target_test)]))
-top3 = in_top_n(prob, target_test, 3)
-top5 = in_top_n(prob, target_test, 5)
-results['titles'] = dict(top1=prec, top3=top3, top5=top5,
-                         loglikelihood=loglikelihood)
-print('title vectors done!')
-
-## Next fit on just the abstracts
-print('Fitting abstract vectors')
-lr.fit(abstract_train, target_train)
-prec = np.mean(lr.predict(abstract_test) == target_test)
-prob = lr.predict_proba(abstract_test)
-loglikelihood = np.sum(np.log([p[t] for p, t in zip(prob, target_test)]))
-top3 = in_top_n(prob, target_test, 3)
-top5 = in_top_n(prob, target_test, 5)
-results['abstracts'] = dict(top1=prec, top3=top3, top5=top5,
-                            loglikelihood=loglikelihood)
-print('abstract vectors done!')
-
-# Now on the combination of titles and abstracts
-print('Fitting abstract + title vectors')
-lr.fit(combined_train, target_train)
-prec = np.mean(lr.predict(combined_test) == target_test)
-prob = lr.predict_proba(combined_test)
-loglikelihood = np.sum(np.log([p[t] for p, t in zip(prob, target_test)]))
-top3 = in_top_n(prob, target_test, 3)
-top5 = in_top_n(prob, target_test, 5)
-results['abstracts+titles'] = dict(
-    top1=prec, top3=top3, top5=top5, loglikelihood=loglikelihood
-)
-print('abstract + title vectors done!')
-
-with open("logistic-regression-embedding-vectors-2019-03-19.pkl", 'wb') as fout:
-    pickle.dump(results, fout)
+    # Next fit on the cocitation features
+    co_ti_ab_train = np.concatenate([title_abstract_train, mc_train], axis=1)
+    del title_abstract_train
+    co_ti_ab_test = np.concatenate([title_abstract_test, mc_test], axis=1)
+    del title_abstract_test
+    
+    print('Fitting title+abstract+cocitation vectors')
+    results['cocitation+title+abstracts'] = train_test(
+        lr, co_ti_ab_train, target_train, co_ti_ab_test, target_test
+    )
+    print(results['cocitation+title+abstracts'])
+    print('title+abstract+cocitation vectors done!')
+    
+    nowdate = str(datetime.now()).split()[0]
+    filename = "logistic-regression-embedding-vectors-{}.pkl".format(nowdate)
+    with open(filename, 'wb') as fout:
+        pickle.dump(results, fout)
