087fe5b934f2f00b6c7c7d5439d99ab5a7cf4293
kev <kokeeffe@mit.edu>
2019-05-01 00:32:28 -0400

    GCN now working

--- analysis/kipf_welling_GCN/GCN_classification.py
+++ analysis/kipf_welling_GCN/GCN_classification.py
@@ -3,27 +3,51 @@
 """
 
 import os, time
+import GCN_utils as u
 
 
 def main():
     
+    N = 10**2   #number of nodes in graph to take
+    #N = 0      #this means take the full dataset
     
-    epochs = 10**3
+    #Options
+    data_made = False     #might want to reuse the data that's been used already
+    delete_data = True   #delete the data file after its been used
+        
+    #Put data into right format
+    if data_made == False:
+        print('Preparing data \n ')
+        if not os.path.exists('data/'):
+            os.system('mkdir data')
+        if not os.path.exists('results'):
+            os.system('mkdir results')
+        u.cast_data_into_right_form(N)
+    
+    #Then do the GCN classification
+    print('\n Now starting classification')
+    epochs = 10**2
     
     #title vectors --- see the train.py file for other command file arguments
-    t1 = time.time()
     print('Starting title vectors')
     os.system('python train.py --dataset arXiv-title --epochs ' + str(epochs))
-    t2 = time.time()
-    print('\n Sim took ' + str((t2-t1)/60.0) + ' mins')
 
     #abstract vectors
-    #print('\n Staring abstract vectors')
-    #os.system('python train.py --dataset arXiv-abstract --epochs ' + str(epochs))
+    print('\n Staring abstract vectors')
+    os.system('python train.py --dataset arXiv-abstract --epochs ' + str(epochs))
     
-    #title and abstracts
-    #print('\n Starting title and abstract vectors')
-    #os.system('python train.py --dataset arXiv-title-abstract -- epochs ' + str(epochs))
+    #fulltext
+    print('\n Starting fulltext vectors')
+    os.system('python train.py --dataset arXiv-fulltext -- epochs ' + str(epochs))
+    
+    #all together
+    print('\n Starting all vectors')
+    os.system('python train.py --dataset arXiv-all -- epochs ' + str(epochs))
+    
+     
+    #Delete the data I don't need
+    if delete_data == True:
+        os.system('rm -r data/')
     
     return
     
--- analysis/kipf_welling_GCN/GCN_utils.py
+++ analysis/kipf_welling_GCN/GCN_utils.py
@@ -5,14 +5,12 @@ sys.path.append('..')
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
-import arxiv_public_data.tests.intra_citation as ia 
+import intra_citation as ia 
 import time, json, gzip
 import pickle as pkl
 from arxiv_public_data.oai_metadata import load_metadata
+from arxiv_public_data.embeddings.util import load_embeddings, fill_zeros
 
-
-                                #Auxiliary
-#-----------------------------------------------------------------------
 
 
 def sync_G_with_metadata(G,m):
@@ -40,7 +38,7 @@ def sync_G_with_metadata(G,m):
             G.add_node(node)
     
     #Check if they're sync'd up
-    print('(#Nodes in G, #articles in meta-data) = ' + str((G.number_of_nodes(),len(m))))
+    print('(# Nodes in G, # articles in meta-data) = ' + str((G.number_of_nodes(),len(m))))
     if G.number_of_nodes() == len(m):
         print('So the syncing worked')
     else:
@@ -94,14 +92,7 @@ def load_titles(G, m, nodes_string, dirname):
     
     #Load the full feature matrix
     filename = dirname + '/title-embedding-usel-2019-03-19.pkl'
-    out = []
-    with open(filename, 'rb') as f:
-        while True:
-            try:
-                out.extend(pkl.load(f))
-            except EOFError as e:
-                break
-    title_vecs = np.array(out)
+    title_vecs =  load_embeddings(filename)['embeddings']
     
     #Then select the subset corresponding to the sub-graph we're examining
     if len(nodes_string) == G.number_of_nodes():
@@ -119,14 +110,7 @@ def load_abstracts(G, m, nodes_string, dirname):
     
     #Load the full feature matrix
     filename = dirname + '/abstract-embedding-usel-2019-03-19.pkl'
-    out = []
-    with open(filename, 'rb') as f:
-        while True:
-            try:
-                out.extend(pkl.load(f))
-            except EOFError as e:
-                break
-    abstract_vecs = np.array(out)
+    abstract_vecs =  load_embeddings(filename)['embeddings']
     
     #Then select the subset corresponding to the sub-graph we're examining
     if len(nodes_string) == G.number_of_nodes():
@@ -143,16 +127,10 @@ def load_abstracts(G, m, nodes_string, dirname):
 def load_fulltext(G, m, nodes_string, dirname):
     
     #Load the full feature matrix
+    dirname = '/home/kokeeffe/research/arxiv-public-datasets/arxiv-data/output/embeddings'
     filename = dirname + '/fulltext-embedding-usel-2-headers-2019-04-05.pkl'
-    out = []
-    with open(filename, 'rb') as f:
-        while True:
-            try:
-                out.extend(pkl.load(f))
-            except EOFError as e:
-                break
-    fulltext_vecs = np.array(out)
-    
+    fulltext_vecs = fill_zeros(load_embeddings(filename, 2))
+
     #Then select the subset corresponding to the sub-graph we're examining
     if len(nodes_string) == G.number_of_nodes():
         return fulltext_vecs
@@ -172,7 +150,8 @@ def load_labels(nodes_string, m):
     return labels_cat
 
 
-def save_data(nodes_int, dirname,vector_type, vector_train, vector_test, vector, G_sub, m):
+def save_data(nodes_int, dirname,vector_type, vector_train, vector_test, vector, labels_train, \
+              labels_test, labels_cat, G_sub, m, cutoff1, cutoff2):
     """ Saves data in format required by Kipfs and Welling
     
         nodes_int = list, list of nodes labeled by integers
@@ -180,7 +159,7 @@ def save_data(nodes_int, dirname,vector_type, vector_train, vector_test, vector,
         vector_type = string, = 'title', 'abstract', 'full-text'
     
     """
-
+    
     #Save vectors
     dirname = 'data'
     fname = dirname + '/ind.arXiv-' + vector_type + '.x'
@@ -229,15 +208,18 @@ def save_data(nodes_int, dirname,vector_type, vector_train, vector_test, vector,
     pkl.dump(graph_dict, open(dirname + '/ind.arXiv-' + vector_type + '.graph', 'wb')) 
     return
     
+    
+    
 
-                                #Main
-#-----------------------------------------------------------------------
+def cast_data_into_right_form(N):
+    """
+    Puts the arXivdata into right form for kipf Welling GCN classifier.
+    
+    N = int, number of nodes in citation graph
+             N = 0 means take all the nodes
+        
+    """
 
-if __name__ == "__main__":
-
-
-    N = 2*10**3  #size of subgraph to be used (using a subraph during testing phase)
-    #N = 0 #This means take all of the data
     
     #Load graph & metadata
     t1 = time.time()
@@ -246,7 +228,7 @@ if __name__ == "__main__":
     dirname = '/home/kokeeffe/research/arxiv-public-datasets/arxiv-data'
     m = load_metadata(dirname + '/oai-arxiv-metadata-2019-03-01.json.gz')
     
-    graph_saved = True    #for debugging, I've already made the full graph
+    graph_saved = False    #for debugging, I've already made the full graph
     if graph_saved == True:
         dirname = 'data'
         G = nx.read_gpickle(dirname + '/full_graph.gpickle')
@@ -277,7 +259,7 @@ if __name__ == "__main__":
     dirname = '/home/kokeeffe/research/arxiv-public-datasets/arxiv-data/output/embeddings'
     title_vecs = load_titles(G, m, nodes_string, dirname)
     abstract_vecs = load_abstracts(G, m, nodes_string, dirname)
-    #fulltext_vecs = load_fulltext(G, m, nodes_string, dirname)
+    fulltext_vecs = load_fulltext(G, m, nodes_string, dirname)
     
     #Load labels
     dirname = '/home/kokeeffe/research/arxiv-public-datasets/arxiv-data'
@@ -293,18 +275,22 @@ if __name__ == "__main__":
     cutoff2 = int(0.9*title_vecs.shape[0])
     title_vec_train, title_vec_test = title_vecs[:cutoff1], title_vecs[cutoff2:]
     abstract_vec_train, abstract_vec_test = abstract_vecs[:cutoff1], abstract_vecs[cutoff2:]
-    #fulltext_vec_train, fulltext_vec_test = fulltext_vecs[:cutoff1], fulltext_vecs[cutoff2:]
+    fulltext_vec_train, fulltext_vec_test = fulltext_vecs[:cutoff1], fulltext_vecs[cutoff2:]
     labels_train, labels_test = labels_cat[:cutoff1], labels_cat[cutoff2:]
 
     #Save data
     dirname = 'data'
-    save_data(nodes_int, dirname, 'title', title_vec_train, title_vec_test, title_vecs, G_sub, m)
-    save_data(nodes_int, dirname, 'abstract', abstract_vec_train, abstract_vec_test, abstract_vecs, G_sub, m)
-    #save_data(nodes_int, dirname, 'fulltext', fulltext_vec_train, fulltext_vec_test, fulltext_vecs, G_sub, m)
+    save_data(nodes_int, dirname, 'title', title_vec_train, title_vec_test, title_vecs,labels_train, labels_test, labels_cat, G_sub, m, cutoff1, cutoff2)
     
-    #Combine title and abstract vecs
-    title_vecs = np.concatenate((title_vecs, abstract_vecs), axis=1)
+    save_data(nodes_int, dirname, 'abstract', abstract_vec_train, abstract_vec_test, abstract_vecs, labels_train, labels_test, labels_cat, G_sub, m, cutoff1, cutoff2)
+    
+    save_data(nodes_int, dirname, 'fulltext', fulltext_vec_train, fulltext_vec_test, fulltext_vecs,labels_train, labels_test, labels_cat, G_sub, m, cutoff1, cutoff2)
+    
+    #Combine all
+    title_vecs = np.concatenate([title_vecs, abstract_vecs, fulltext_vecs], axis=1)
     title_vec_train, title_vec_test = title_vecs[:cutoff1], title_vecs[cutoff2:]
-    #save_data(nodes_int, dirname, 'title-abstract', title_vec_train, title_vec_test, title_vecs, G_sub, m)
+    save_data(nodes_int, dirname, 'all', title_vec_train, title_vec_test, title_vecs, labels_train, labels_test, labels_cat, G_sub, m, cutoff1, cutoff2)
     t2 = time.time()
     print( 'Saving data took ' + str((t2-t1)/60.0) + ' mins')
+    
+    return
