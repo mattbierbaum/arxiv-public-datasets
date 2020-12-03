f64b6ddefb6ed60dbc640ca1d24fff7079def094
Quynh Nguyen <quynhneo@users.noreply.github.com>
2020-12-02 20:02:43 -0500

    adding comments

--- bin/cocitations.py
+++ bin/cocitations.py
@@ -8,17 +8,15 @@ if __name__ == '__main__':
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
     args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
     opts_args =dict(zip(opts, args))
-    if '-dir' in opts_args:
+    if '-dir' in opts_args:  # option for directory of full text files
         txt_dir = opts_args['-dir']
     else:
-        txt_dir = DIR_FULLTEXT
+        txt_dir = DIR_FULLTEXT  # default directory
 
-    if '-N' in opts_args:
+    if '-N' in opts_args:  # option for number of cpus
         processes = opts_args['-N']
     else:
-        processes = multiprocessing.cpu_count()
-    print(processes)
-    print(txt_dir)
+        processes = multiprocessing.cpu_count()  # default: all cpu available
 
     cites = internal_citations.citation_list_parallel(N=processes, directory=txt_dir)
     internal_citations.save_to_default_location(cites)
