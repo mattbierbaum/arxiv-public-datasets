27ddf1c00a0470271c1d8b4f1d4f2b054223ae0f
Quynh Nguyen <qmn203@nyu.edu>
2020-12-08 22:35:57 -0500

    feat: parse argument with argparse

--- bin/cocitations.py
+++ bin/cocitations.py
@@ -1,22 +1,16 @@
 import multiprocessing
 from arxiv_public_data import internal_citations
 from arxiv_public_data.config import DIR_FULLTEXT
+from argparse import ArgumentParser
 
 if __name__ == '__main__':
     import sys
 
-    opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
-    args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
-    opts_args = dict(zip(opts, args))
-    if '-dir' in opts_args:  # option for directory of full text files
-        txt_dir = opts_args['-dir']
-    else:
-        txt_dir = DIR_FULLTEXT  # default directory
+    parser = ArgumentParser(description="""Generate cocitation network from fulltext and save as a json file""")
+    parser.add_argument("-N", type=int, default=multiprocessing.cpu_count(), help="OPTIONAL number of CPUs, default all available")
+    parser.add_argument("--dir", type=str, default=DIR_FULLTEXT, help="OPTIONAL directory containing articles in text format, default "+ DIR_FULLTEXT)
 
-    if '-N' in opts_args:  # option for number of cpus
-        processes = opts_args['-N']
-    else:
-        processes = multiprocessing.cpu_count()  # default: all cpu available
+    args = parser.parse_args()
 
-    cites = internal_citations.citation_list_parallel(N=processes, directory=txt_dir)
+    cites = internal_citations.citation_list_parallel(N=args.N, directory=args.dir)
     internal_citations.save_to_default_location(cites)
